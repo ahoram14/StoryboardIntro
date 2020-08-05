e31ef6fc7396d50659f43f24f7e47dba00098e1f
ahoram14 <69254557+ahoram14@users.noreply.github.com>
2020-08-05 11:33:29 -0400

    My first Xcode Project

---
+++ Storyboard Intro/Storyboard Intro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Storyboard Intro
+//
+//  Created by Amira Horam on 8/5/20.
+//  Copyright © 2020 Amira Horam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       print("This a print statement - testing testing")
+    }
+
+
+}
+
